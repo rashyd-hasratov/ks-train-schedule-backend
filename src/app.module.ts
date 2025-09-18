5d50121e75c6d61a25eeff0b96afbee96e164224
rashyd-hasratov <rashydlezgi@gmail.com>
2025-09-18 22:47:43 +0300

    configured sequelize, connected to postgres db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      uri: process.env.DB_URI,
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
